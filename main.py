67217e3adbf557bb5b6948f31380885af6f1e9d2
João Henrique Vieira <jonhenrique007@gmail.com>
2024-10-07 20:39:52 -0300

    Creating main loop Create Game class

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup start')
-pygame.init()
-window = pygame.display.set_mode((600, 480))
-print('Setup end')
-
-while True:
-#     Check for all events..
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game=Game()
+game.run()
