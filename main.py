ca97ac8d5f748c59249cc9d405ca40284c6fae95
João Henrique Vieira <jonhenrique007@gmail.com>
2024-10-07 20:03:51 -0300

    teste

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window = pygame.display.set_mode((600, 480))
 print('Setup end')
 
 while True:
-#     Check for all events
+#     Check for all events..
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
